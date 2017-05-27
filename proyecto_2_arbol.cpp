befd2d9c481b29e0d1101321b3664e7e316fec8c
tatanmejia <jhotan_22m@hotmail.com>
2017-05-27 15:06:04 -0500

    correcciones funciones

--- proyecto_2_arbol.cpp
+++ proyecto_2_arbol.cpp
@@ -12,6 +12,8 @@ void menu();
 int pedirDatos();
 void insertarElementosArbol(Nodo *&Arbol, int n);
 void imprimirArbol(Nodo *Arbol, int cont);
+bool busqueda(Nodo *, int);
+void modificar_nodo(Nodo *&, int);
 void post_orden(Nodo *);
 void pre_orden(Nodo *);
 void in_orden(Nodo *);
@@ -23,7 +25,7 @@ int main(){
 
 void menu(){
 	Nodo *Arbol = NULL;
-	int opcion, n, numDatos, cont = 0;
+	int opcion, n, numDatos, cont = 0, x, a;
 	do{
 		printf("\n*****MENU*****\n");
 		printf("\n1. Insertar Elementos en el Arbol");
@@ -46,14 +48,47 @@ void menu(){
 					numDatos -= 1;
 				}
 				break;
-			case 2:
-				printf("\nMostar Arbol\n");
-				imprimirArbol(Arbol, cont);
-				break;
+			case 2:{
+            system("cls");
+            printf("1. Mostrar arbol en preorden\n");
+            printf("2. Mostrar arbol en inorden\n");
+            printf("3. Mostrar arbol en posorden\n");
+            printf("Digite opcion: ");
+            scanf("%d",&x);
+            switch(x){
+                case 1:{
+                    pre_orden(Arbol);
+                    printf("\n");
+                }break;
 
-            case 3:
-                busqueda();
+                case 2:{
+                    in_orden(Arbol);
+                    printf("\n");
+                }break;
 
+                case 3:{
+                    post_orden(Arbol);
+                    printf("\n");
+                }break;
+                default: printf("Opcion incorrecta\n");
+            }
+            }break;
+
+            case 3:{
+            printf("Ingrese dato que desea buscar: ");
+            scanf("%d",&a);
+            if(busqueda(Arbol,a)==true){
+                printf("dato encontrado\n");
+            }else{
+                printf("dato no encontrado\n");
+            }
+            }break;
+
+            case 4:{
+            printf("Ingrese dato que desea modificar: ");
+            scanf("%d",&a);
+            modificar_nodo(Arbol,a);
+            }break;
 
 			default:
 				printf("\nNo es una opcion valida");
@@ -125,7 +160,7 @@ void post_orden(Nodo *Arbol){
     else{
         post_orden(Arbol->izquierdo);
         post_orden(Arbol->derecho);
-        printf("%d",Arbol->dato " - ");
+        printf("%d",Arbol->dato);
     }
 }
 
@@ -135,7 +170,7 @@ void pre_orden(Nodo *Arbol){
         return;
     }
     else{
-        printf("%d",Arbol->dato " - ");
+        printf("%d",Arbol->dato);
         pre_orden(Arbol->izquierdo);
         pre_orden(Arbol->derecho);
 
@@ -149,8 +184,30 @@ void in_orden(Nodo *Arbol){
     }
     else{
         in_orden(Arbol->izquierdo);
-        printf("%d",Arbol->dato " - ");
+        printf("%d",Arbol->dato);
         in_orden(Arbol->derecho);
 
+    }
+}
+
+
+void modificar_nodo(nodo *&Arbol, int a){
+    int x, y;
+    if(arbol==NULL){
+        return;
+    }else{
+        if(arbol->dato==a){
+            printf("Ingrese nuevo dato: ");
+            scanf("%d",&x);
+            arbol->dato=x;
+        }else{
+            printf("Buscar 1.Izquierda  2.Derecha: ");
+            scanf("%d",&y);
+            if(y==1){
+                busqueda(Arbol->izq,a);
+            }else{
+                busqueda(Arbol->der,a);
+            }
+        }
     }
 }
